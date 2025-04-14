0796b832e87cdf7a2eb64a49197e722f0048d979
macAji <ajienergystar@gmail.com>
2025-04-14 17:33:36 +0700

    Main - Contracts - Create ClaimsListContracts Code

---
+++ ListDataApp/Contracts/ClaimsListContracts.swift
@@ -0,0 +1,25 @@
+//  Created By Aji Prakosa 2025
+
+import Foundation
+import SwiftUI
+
+protocol ClaimsListViewProtocol: AnyObject {
+    func showLoading()
+    func hideLoading()
+    func showClaims(_ claims: [Claim])
+    func showError(_ message: String)
+}
+
+protocol ClaimsListPresenterProtocol {
+    func onAppear()
+    func didSelectClaim(_ claim: Claim)
+    func searchClaims(with query: String)
+}
+
+protocol ClaimsListInteractorInputProtocol {
+    func fetchClaims() async throws -> [Claim]
+}
+
+protocol ClaimsListRouterProtocol {
+    func navigateToClaimDetail(with claim: Claim) -> AnyView
+}
