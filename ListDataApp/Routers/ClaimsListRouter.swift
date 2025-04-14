9bedcd66790363b02cdae9b540ef48ae47a023bd
macAji <ajienergystar@gmail.com>
2025-04-14 19:33:03 +0700

    Main - Routers - Create ClaimsListRouter Code

---
+++ ListDataApp/Routers/ClaimsListRouter.swift
@@ -0,0 +1,21 @@
+//  Created By Aji Prakosa 2025
+
+import Foundation
+import SwiftUI
+
+class ClaimsListRouter: ClaimsListRouterProtocol {
+    func navigateToClaimDetail(with claim: Claim) -> AnyView {
+        let interactor = ClaimDetailInteractor(claim: claim)
+        let presenter = ClaimDetailPresenter(interactor: interactor)
+        return AnyView(ClaimDetailView(presenter: presenter))
+    }
+    
+    static func createModule() -> some View {
+        let interactor = ClaimsListInteractor()
+        let router = ClaimsListRouter()
+        let presenter = ClaimsListPresenter(interactor: interactor, router: router)
+        let view = ClaimsListView(presenter: presenter)
+        presenter.view = view
+        return view
+    }
+}
