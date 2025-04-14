8216ec4aaad8585ca25fa3720de0301af7c59410
macAji <ajienergystar@gmail.com>
2025-04-14 15:50:19 +0700

    Initial Commit

---
+++ ListDataApp/ListDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListDataAppApp.swift
+//  ListDataApp
+//
+//  Created by mac on 14/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ListDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
