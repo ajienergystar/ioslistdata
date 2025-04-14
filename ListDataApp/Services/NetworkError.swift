b2462f13b5f4c48c890e323a3af5568a08c4487e
macAji <ajienergystar@gmail.com>
2025-04-14 17:23:27 +0700

    Main - Services - update NetworkError Code

--- ListDataApp/Services/NetworkError.swift
+++ ListDataApp/Services/NetworkError.swift
@@ -1,8 +1,19 @@
 //  Created By Aji Prakosa 2025
 
-enum NetworkError: Error {
+import Foundation
+
+enum NetworkError: Error, LocalizedError {
     case invalidURL
     case noData
     case decodingError
     case serverError(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL: return "Invalid URL"
+        case .noData: return "No data received"
+        case .decodingError: return "Failed to decode data"
+        case .serverError(let error): return "Server error: \(error.localizedDescription)"
+        }
+    }
 }
