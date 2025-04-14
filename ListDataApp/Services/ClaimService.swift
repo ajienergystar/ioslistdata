c28f68b8802c2780823c3c67b032c1fafe181f37
macAji <ajienergystar@gmail.com>
2025-04-14 17:14:58 +0700

    Main - Services - update ClaimService Code

--- ListDataApp/Services/ClaimService.swift
+++ ListDataApp/Services/ClaimService.swift
@@ -1,8 +1,22 @@
-//
-//  ClaimService.swift
-//  ListDataApp
-//
-//  Created by mac on 14/4/25.
-//
+//  Created By Aji Prakosa 2025
 
-import Foundation
+import Alamofire
+
+protocol ClaimServiceProtocol {
+    func fetchClaims(completion: @escaping (Result<[Claim], NetworkError>) -> Void)
+}
+
+class ClaimService: ClaimServiceProtocol {
+    private let baseURL = "https://jsonplaceholder.typicode.com/posts"
+    
+    func fetchClaims(completion: @escaping (Result<[Claim], NetworkError>) -> Void) {
+        AF.request(baseURL).validate().responseDecodable(of: [Claim].self) { response in
+            switch response.result {
+            case .success(let claims):
+                completion(.success(claims))
+            case .failure(let error):
+                completion(.failure(.serverError(error)))
+            }
+        }
+    }
+}
